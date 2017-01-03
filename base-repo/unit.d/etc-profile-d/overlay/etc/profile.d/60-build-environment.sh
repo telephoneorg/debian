0cbae310709a56cc5fa611871fc69610db000e21
Joe Black <joeblack949@gmail.com>
2017-01-02 23:30:02 -0500

    fix for environment

--- base-repo/unit.d/etc-profile-d/overlay/etc/profile.d/60-build-environment.sh
+++ base-repo/unit.d/etc-profile-d/overlay/etc/profile.d/60-build-environment.sh
@@ -1,5 +1,2 @@
 # populate environment
-if [[ -f /etc/environment.d ]]; then
-    eval $(cat /etc/environment.d/*)
-    export $(cat /etc/environment.d/* | grep -v ^# | grep '[^[:blank:]]' | cut -d= -f1)
-fi
+. /etc/environment
