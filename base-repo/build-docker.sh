74583137adf22b76e364e37926fbf31124d2de35
Joe Black <joeblack949@gmail.com>
2017-02-02 15:46:10 -0500

    fixed image name

--- base-repo/build-docker.sh
+++ base-repo/build-docker.sh
@@ -4,5 +4,5 @@ set -e
 
 base=$(dirname $0)
 
-docker build -t $DOCKER_USER/latest $base
-docker run -i --rm $DOCKER_USER/latest bash -lc env
+docker build -t $DOCKER_USER/debian:latest $base
+docker run -i --rm $DOCKER_USER/debian:latest bash -lc env
